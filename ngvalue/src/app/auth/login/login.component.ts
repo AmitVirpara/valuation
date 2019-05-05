ee8f1a85e3251aea27faf4f0e0a99a8d8ffba4d2
Amit <amtvirpara@gmail.com>
2019-05-05 14:24:48 +0530

    create login page design on ng

--- ngvalue/src/app/auth/login/login.component.ts
+++ ngvalue/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  title = 'Ng Value';
+  frmGrpLogin: FormGroup;
+  userlogin:any = {
+    name:'',
+    email:'',
+    password:''
+  };
+
+  get frmgrplogin() { return this.frmGrpLogin; }
+  get name() { return this.frmGrpLogin.controls.name; }
+  get email() { return this.userlogin.email; }
+  get password() { return this.frmGrpLogin.controls.password; }
+
+  constructor(private frmBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.frmGrpLogin = this.frmBuilder.group({
+      name : ['', [ Validators.required, Validators.minLength(4) ] ],
+      password : ['', [ Validators.required, Validators.minLength(4) ] ],
+    }
+    );
   }
 
+  onSubmit(){
+    console.log('form submit', this.frmGrpLogin)
+  }
 }
